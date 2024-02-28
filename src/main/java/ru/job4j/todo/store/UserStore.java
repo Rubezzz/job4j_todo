820d4005617a34d7ba42624ef2d0ddc960829bed
Rubezzz <inigor1@mail.ru>
2024-02-28 21:13:29 +0700

    6. Лямбды и шаблон Command [#49295 #483515]

--- src/main/java/ru/job4j/todo/store/UserStore.java
+++ src/main/java/ru/job4j/todo/store/UserStore.java
@@ -1,6 +1,9 @@
 package ru.job4j.todo.store;
 
-import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
@@ -8,20 +11,27 @@ import java.util.Map;
 import java.util.Optional;
 
 @Repository
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserStore implements UserRepository {
 
+    @NonNull
     private final CrudRepository crudRepository;
+    private final Logger logger = LoggerFactory.getLogger(TaskStore.class);
 
     @Override
     public Optional<User> save(User user) {
-        crudRepository.run(
-                session -> {
-                    session.persist(user);
-                    return 0;
-                }
-        );
-        return Optional.of(user);
+        try {
+            crudRepository.run(
+                    session -> {
+                        session.persist(user);
+                        return 0;
+                    }
+            );
+            return Optional.of(user);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return Optional.empty();
     }
 
     @Override
